ee2b3a3d076df7e0ee3d51ce3f767a7fc49d14c0
whosAle <abelgrave95@gmail.com>
2019-04-26 11:17:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(username, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: username,
+      email: userEmail
+    })
+  })
+  .then(response => response.json())
+  .then(jsonObject => displayID(jsonObject))
+  .catch(error => displayError(error.message));
+}
+
+function displayID(json) {
+  return document.querySelector("body").innerHTML += `<p>Your ID is: ${json.id}.`;
+}
+
+function displayError(error) {
+  return document.querySelector("body").innerHTML += `<p>Unauthorized Access. Error: </p> <p>${error}</p>`;
+}
